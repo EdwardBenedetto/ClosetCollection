0432edef7f932b7de5f8b59a8efa1587f4b2ed0f
Edward Benedetto <edwardbenedetto@gmail.com>
2022-11-10 16:45:57 -0500

    formatting resolution

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,23 +1,23 @@
 const Comment = require("../models/Comment");
 
 module.exports = {
- 
+
   createComment: async (req, res) => {
     try {
-          await Comment.create({
+      await Comment.create({
         comment: req.body.comment,
-         likes: 0,
+        likes: 0,
         post: req.params.id,
         user: req.user.userName,
-        createdBy:req.user.userName,
+        createdBy: req.user.userName,
         createdbyID: req.user.id,
       });
-   
+
       console.log("Comment has been added!");
-      res.redirect("/post/"+req.params.id);
-      } 
-      
-      catch (err) {
+      res.redirect("/post/" + req.params.id);
+    }
+
+    catch (err) {
       console.log(err);
     }
   },
@@ -28,9 +28,9 @@ module.exports = {
       // let comment = await Comment.findById({ _id: req.params.id });
       await Comment.deleteOne({ _id: req.params.commentid });
       console.log("Deleted Comment");
-      res.redirect("/post/"+req.params.postid); 
+      res.redirect("/post/" + req.params.postid);
     } catch (err) {
-      res.redirect("/post/"+req.params.postid);
+      res.redirect("/post/" + req.params.postid);
     }
   },
 };
